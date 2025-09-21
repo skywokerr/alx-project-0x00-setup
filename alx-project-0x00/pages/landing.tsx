5032fce32fe72b8acf4abfaaddb8628d3f7cae01
Michael Atolwa <atolwamichael7@gmail.com>
2025-09-21 23:20:45 +0300

    lading.tsx update

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,7 +1,13 @@
+import Card from "@/components/Card"
+
 const Landing: React.FC = () => {
   return (
     <div>
       <h1 className=" text-xl font-extralight">Landing Page</h1>
+      {/* Using the component multiple times! */}
+      <Card />
+      <Card />
+      <Card />
     </div>
   )
 }
