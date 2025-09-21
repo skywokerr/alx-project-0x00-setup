48787ce473373b246c72976a706dc358a815c7d1
Michael Atolwa <atolwamichael7@gmail.com>
2025-09-21 23:23:38 +0300

    pill update

--- alx-project-0x00/components/Pill.tsx
+++ alx-project-0x00/components/Pill.tsx
@@ -1,7 +1,12 @@
-const Pill: React.FC = () => {
+// 1. Import the interface
+import { PillProps } from "@/interfaces";
+
+// 2. & 3. Apply the interface and destructure the title prop
+const Pill: React.FC<PillProps> = ({ title }) => {
   return (
     <div className=" flex justify-center  items-center bg-[#F9F9F9] px-3 w-auto h-[27px] rounded-full">
-      <p className=" text-sm ">Title</p>
+      {/* 4. Use the dynamic title prop */}
+      <p className=" text-sm ">{title}</p>
     </div>
   )
 }
